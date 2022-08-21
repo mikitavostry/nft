42f9552c43f3b9e6968df01ac7c4abcbef295f93
Mikita <vostrymikita@gmail.com>
2022-08-22 01:10:40 +0200

    first commit

---
+++ scripts/upload_to_pinata.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+from urllib import response
+import requests
+
+PINATA_BASE_URL = "https://api.pinata.cloud"
+endpoint = "/pinning/pinFileToIPFS"
+filepath = "./img/pug.png"
+filename = filepath.split("/")[-1:][0]
+headers = {
+    "pinata_api_key": os.getenv("PINATA_API_KEY"),
+    "pinata_secret_api_key": os.getenv("PINATA_API_SECRET"),
+}
+
+
+def main():
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        respond = requests.post(
+            PINATA_BASE_URL + endpoint,
+            files={"file": (filename, image_binary)},
+            headers=headers,
+        )
+        print(respond.json())
